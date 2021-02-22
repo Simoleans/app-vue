c9eac01883aeaf388f9d64c616a09a6585fb4b4a
Fran <francisco20990@gmail.com>
2021-02-21 20:52:05 -0400

    Menú | menu repsonsive listo

--- src/components/Dashboard/NavBar.vue
+++ src/components/Dashboard/NavBar.vue
@@ -1,7 +1,7 @@
 <template>
 	<!-- This example requires Tailwind CSS v2.0+ -->
-<nav class="bg-gray-800">
-  <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+<nav class="bg-gray-800" v-click-away="closeMenuMobile">
+  <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8" v-click-away="closeMenuProfile">
     <div class="relative flex items-center justify-between h-16">
       <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
         <!-- botón movil-->
@@ -24,9 +24,9 @@
         </div>
         <div class="hidden sm:block sm:ml-6">
           <div class="flex space-x-4">
-            <router-link  to="/dashboard" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Inicio</router-link>
-            <router-link  :to="{ name: 'calculate', params: { }}" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Calculadora</router-link>
-            <router-link  :to="{ name: 'pagos', params: { }}" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Mis Pagos</router-link>
+            <router-link to="/dashboard" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Inicio</router-link>
+            <router-link :to="{ name: 'calculate', params: { }}" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Calculadora</router-link>
+            <router-link :to="{ name: 'pagos', params: { }}" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Mis Pagos</router-link>
           </div>
         </div>
       </div>
@@ -42,13 +42,13 @@
         <!-- Profile dropdown -->
         <div class="ml-3 relative">
           <div>
-            <button @click="showMenuProfile" v-click-away="closeMenuProfile" class="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white" id="user-menu" aria-haspopup="true">
+            <button @click="showMenuProfile"  class="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white" id="user-menu" aria-haspopup="true">
               <span class="sr-only">Open user menu</span>
               <img class="h-8 w-8 rounded-full" v-bind:src="user.photoURL ?? urlGuest" alt="Perfil">
             </button>
           </div>
           
-          <div v-show="profileMenu" class="origin-top-right z-40 absolute right-0 mt-2 w-30 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5" role="menu" aria-orientation="vertical" aria-labelledby="user-menu">
+          <div v-show="profileMenu" class="origin-top-right z-50 absolute right-0 mt-2 w-30 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5" role="menu" aria-orientation="vertical" aria-labelledby="user-menu">
             <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">{{ user.email ?? 'Invitado' }}</a>
             <a href="#" @click="logOut" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">Salir</a>
           </div>
@@ -60,22 +60,12 @@
 <!-- menú mobile -->
   <div class="sm:hidden" v-show="showMenu">
     <div class="px-2 pt-2 pb-3 space-y-1">
-      <a href="#" class="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium">construcción</a>
-      <!-- <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Team</a>
-      <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Projects</a>
-      <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Calendar</a> -->
+      <router-link to="/dashboard" class="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium">Inicio</router-link>
+      <router-link :to="{ name: 'calculate', params: { }}" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Calculadora</router-link>
+      <router-link :to="{ name: 'pagos', params: { }}" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Pagos</router-link>
     </div>
   </div>
 
- <!-- menú desktop -->
-  <div class="hidden sm:hidden">
-    <!-- <div class="px-2 pt-2 pb-3 space-y-1">
-      <a href="#" class="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium">Dashboard</a>
-      <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Team</a>
-      <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Projects</a>
-      <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Calendar</a>
-    </div> -->
-  </div>
 </nav>
 </template>
 
@@ -84,6 +74,7 @@ import { directive } from "vue3-click-away";
 import { firebase } from '@/components/firebaseInit'
 import { ref } from 'vue'
 import { data } from "@/composables/DataAuth";
+import { useRouter } from 'vue-router'
 
 export default {
   directives: {
@@ -92,33 +83,49 @@ export default {
   setup() {
     
     const user = data;
+    const showMenu = ref(false);
+    const profileMenu = ref(false);
+    const router = useRouter();
 
     let urlGuest = 'https://s2.googleusercontent.com/s2/favicons?domain=google.com&sz=32';
 
-    return {
-      profileMenu : ref(false),
-      showMenu : false,
-      user,
-      urlGuest
+    const handleShowMenu = () => {
+      showMenu.value = !showMenu.value;
+      profileMenu.value = false;
     }
-  },
-  methods : {
-    showMenuProfile() {
-      this.profileMenu = !this.profileMenu
-    },
-    closeMenuProfile() {
-      this.profileMenu = false;
-    },
-    handleShowMenu() {
-      this.showMenu = !this.showMenu
-    },
-    logOut() {
+
+    const closeMenuMobile = () => {
+      showMenu.value = false;
+    }
+
+    const logOut = () => {
       firebase.auth().signOut().then(() => {
         firebase.auth().onAuthStateChanged(() => {
-          this.$router.push('/')
+          router.push('/')
         })
       })
     }
+
+    const showMenuProfile = () => {
+      profileMenu.value = !profileMenu.value
+      showMenu.value = false;
+    }
+
+    return {
+      profileMenu,
+      showMenu,
+      user,
+      urlGuest,
+      handleShowMenu,
+      closeMenuMobile,
+      logOut,
+      showMenuProfile
+    }
+  },
+  methods : {
+    closeMenuProfile() {
+      this.profileMenu = false;
+    },
   },
 }
 </script>
